bbc6e715cead9bdfaef19e834a1f1bcf59c546b0
jamel186 <jamel20009@gmail.com>
2026-07-16 13:36:44 -0400

    Update data.js

--- data.js
+++ data.js
@@ -330,7 +330,7 @@ const BIRDS = [
   {
     commonName: "Clapper Rail",
     scientificName: "Piranga olivacea",
-    dateFirstSeen: "2020-01-02",
+    dateFirstSeen: "2021-01-02",
     location: "Huntley Meadows, VA, USA",
     habitat: ["marsh"],
     notes: "Much smaller than I imagined. Looking at them running along the shore and away from the waves was quite cute.",
@@ -340,7 +340,17 @@ const BIRDS = [
   {
     commonName: "Merlin",
     scientificName: "Piranga olivacea",
-    dateFirstSeen: "2020-01-02",
+    dateFirstSeen: "2021-01-02",
+    location: "Huntley Meadows, VA, USA",
+    habitat: ["marsh"],
+    notes: "Much smaller than I imagined. Looking at them running along the shore and away from the waves was quite cute.",
+    photos: [{ file: "sanderlingFlock.JPG", caption: "Flock of Sanderlings on shore" }],
+    videoUrl: ""
+  },
+  {
+    commonName: "Sora",
+    scientificName: "Piranga olivacea",
+    dateFirstSeen: "2021-01-18",
     location: "Huntley Meadows, VA, USA",
     habitat: ["marsh"],
     notes: "Much smaller than I imagined. Looking at them running along the shore and away from the waves was quite cute.",
