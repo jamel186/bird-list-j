7cbfbd6a168315dd19f4fd15e981586102b13a9d
jamel186 <jamel20009@gmail.com>
2026-07-25 12:09:52 -0400

    Update data.js

--- data.js
+++ data.js
@@ -902,7 +902,7 @@ const BIRDS = [
     location: "Bomun Lake, Gyeongsangbuk-do, South Korea",
     habitat: ["forest"],
     notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
-    photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
+    photos: [{ file: "daurian.JPG", caption: "Female or juvenile sitting in a small tree in the mountains" }],
     videoUrl: ""
   },
    {
@@ -911,7 +911,7 @@ const BIRDS = [
     dateFirstSeen: "2021-07-13",
     location: "Bomun Lake, Gyeongsangbuk-do, South Korea",
     habitat: ["forest"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
+    notes: "Supposedly a common Woodpecker in Korea. Comparable to the Downy Woodpecker in Virginia. However, I have only seen once. Was associating with Asian Tits by a lake.",
     photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
     videoUrl: ""
   },
@@ -921,7 +921,7 @@ const BIRDS = [
     dateFirstSeen: "2021-07-13",
     location: "Bomun Lake, Gyeongsangbuk-do, South Korea",
     habitat: ["open water"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
+    notes: "A common Cormorant in Korea. Found in both freshwater and saltwater habitats.",
     photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
     videoUrl: ""
   },
@@ -931,7 +931,7 @@ const BIRDS = [
     dateFirstSeen: "2021-07-13",
     location: "Bomun Lake, Gyeongsangbuk-do, South Korea",
     habitat: ["open water", "marsh"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
+    notes: "A common small egret of Korea. Found mainly in freshwater habitats. Many live along Cheonggyecheon in Seoul. Similar to the Snowy Egret of America.",
     photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
     videoUrl: ""
   },
@@ -941,7 +941,7 @@ const BIRDS = [
     dateFirstSeen: "2021-07-13",
     location: "Bomun Lake, Gyeongsangbuk-do, South Korea",
     habitat: ["marsh", "open water"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
+    notes: "Very common during the summer, replacing the Mallard. Lives in all types of freshwater habitats.",
     photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
     videoUrl: ""
   },
@@ -951,7 +951,7 @@ const BIRDS = [
     dateFirstSeen: "2021-07-14",
     location: "Bomun Lake, Gyeongsangbuk-do, South Korea",
     habitat: ["forest"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
+    notes: "Very common. I have seen many in city parks and forests in Korea. Acts like a Chickadee, creating mixed species flocks around it.",
     photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
     videoUrl: ""
   },
@@ -961,8 +961,8 @@ const BIRDS = [
     dateFirstSeen: "2021-07-14",
     location: "Bomun Lake, Gyeongsangbuk-do, South Korea",
     habitat: ["forest"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
-    photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
+    notes: "Common in forests in Korea, singing their classic cuckoo song. Usually sings very early in the morning.",
+    photos: [{ file: "daurian.JPG", caption: "Singing its song early in the morning" }],
     videoUrl: ""
   },
   {
@@ -971,7 +971,7 @@ const BIRDS = [
     dateFirstSeen: "2021-07-14",
     location: "Bomun Lake, Gyeongsangbuk-do, South Korea",
     habitat: ["forest"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
+    notes: "Common in mountain forests in Korea. Sings an ethereal flute like song and makes cat-like calls. A bright yellow bird with a black eye mask.",
     photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
     videoUrl: ""
   },
@@ -981,8 +981,8 @@ const BIRDS = [
     dateFirstSeen: "2021-07-14",
     location: "Somewhere on the coast, Gyeongsangbuk-do, South Korea",
     habitat: ["forest"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
-    photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
+    notes: "Very common on the shores of Korea during the summer, both on rocky shores and mudflats.",
+    photos: [{ file: "daurian.JPG", caption: "On the mudflats feeding on crabs" }],
     videoUrl: ""
   },
   {
@@ -991,7 +991,7 @@ const BIRDS = [
     dateFirstSeen: "2021-07-16",
     location: "Seoul Forest, Seoul, South Korea",
     habitat: ["marsh"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
+    notes: "Common in marshes and rice fields in Korea. Comparable to a Green Heron or Night Heron.",
     photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
     videoUrl: ""
   },
@@ -1001,7 +1001,7 @@ const BIRDS = [
     dateFirstSeen: "2021-07-17",
     location: "Bukhansan National Park, Seoul, South Korea",
     habitat: ["forest"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
+    notes: "Uncommon in deep mountain forests in Korea. Have seen one at Bukhansan National Park in the middle of the trail.",
     photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
     videoUrl: ""
   },
@@ -1011,8 +1011,9 @@ const BIRDS = [
     dateFirstSeen: "2021-07-22",
     location: "Namsan Outdoor Botanical Garden, Seoul, South Korea",
     habitat: ["forest", "meadow"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
-    photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
+    notes: "Uncommmon in forests and open areas in Korea. Seeing the male in person was incredible. Surprisingly I saw it in a park in the middle of the city.",
+    photos: [{ file: "daurian.JPG", caption: "An elegant male with its very long tail" },
+            { file: "daurian.JPG", caption: "An understated female sitting in a nearby bush" }],
     videoUrl: ""
   },
   {
@@ -1021,7 +1022,7 @@ const BIRDS = [
     dateFirstSeen: "2021-07-22",
     location: "Namsan Outdoor Botanical Garden, Seoul, South Korea",
     habitat: ["scrub"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
+    notes: "Very common. Exists in city parks in the bushes and in farmland along the edges of the fields. Very loud and active.",
     photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
     videoUrl: ""
   },
@@ -1031,7 +1032,7 @@ const BIRDS = [
     dateFirstSeen: "2021-07-30",
     location: "Olympic Park, Seoul, South Korea",
     habitat: ["open field"],
-    notes: "Very common. I have seen many in city parks in Korea. They often trick me into thinking they are other flycatchers.",
+    notes: "Common in lawns across Korea. Found a couple while birding in the Olympic Park, which has many open lawns.",
     photos: [{ file: "daurian.JPG", caption: "In Holly Tree, backyard" }],
     videoUrl: ""
   },
